cec9975df72d2c0f48045150507dd7dae0742d0d
Ryan <ryangabriel_cap@hotmail.com>
2024-07-24 22:39:52 +0100

    Refatorando rotas e adicionando rota de pedidos ao AppLayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,21 +4,23 @@ import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { Order } from "./pages/app/orders/order";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Order /> },
+    ],
   },
   {
     path: "/",
     element: <AuthLayout />,
-    children: [{ path: "/sign-in", element: <SignIn /> }],
-  },
-  {
-    path: "/",
-    element: <AuthLayout />,
-    children: [{ path: "/sign-up", element: <SignUp /> }],
+    children: [
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
   },
 ]);
