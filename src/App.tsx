4aeaa4072234268a888f9c01e6de989262d36e4e
Ryan <ryangabriel_cap@hotmail.com>
2024-07-24 19:25:03 +0100

    Alterações de configuração da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import "./global.css"
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
