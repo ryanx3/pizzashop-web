de9641482ebb1c09fb8a295d497461d7dfa1f5b2
Ryan <ryangabriel_cap@hotmail.com>
2024-07-24 20:07:28 +0100

    Estilizado página SignUp com referência da página SignIn e adicionado página na rota do app

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/signIn";
+import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -14,5 +15,10 @@ export const router = createBrowserRouter([
     path: "/",
     element: <AuthLayout />,
     children: [{ path: "/sign-in", element: <SignIn /> }],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/sign-up", element: <SignUp /> }],
   },
 ]);
