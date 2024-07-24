1a1b51c471c3ae3f11eb1588dae60f9ad3b3d301
Ryan <ryangabriel_cap@hotmail.comn>
2024-07-24 19:40:28 +0100

    Adicionado Sonner a função de envio do formulário. Feito também o tratamento de erros com try catch.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
-import { useForm } from "react-hook-form";
 
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const signInSchema = z.object({
@@ -21,8 +21,18 @@ export function SignIn() {
   } = useForm<SignInForm>({});
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para o seu email.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais inválidas.")
+    }
   }
 
   return (
