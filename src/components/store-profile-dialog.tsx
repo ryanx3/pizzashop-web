e7e815e87918a44a8c1d55f90a8729c3e4f199c5
Ryan <ryangabriel_cap@hotmail.com>
2024-07-30 17:26:02 +0100

    feat: Criado componente de Modal para alterar os dados dos restaurantes

---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,74 @@
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
+import { DialogContent, DialogFooter, DialogHeader } from "./ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { useQuery } from "@tanstack/react-query";
+import { getManagedRestaunt } from "@/api/get-managed-restaurant";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+});
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>;
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaunt,
+  });
+
+  const { register, handleSubmit, formState } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <form action="">
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input className="col-span-3" id="name" {...register("name")} />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register("description")}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="button" variant="ghost">
+            Cancelar
+          </Button>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  );
+}
