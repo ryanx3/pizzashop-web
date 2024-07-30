86d202258d5e5fafe5af0cb9ce4594c1409dba2f
Ryan <ryangabriel_cap@hotmail.com>
2024-07-30 16:18:27 +0100

    feat: Adicionando função para realizar o cadastro de novos restaurantes utilizando o useMutation, enviando o email que foi realizado o login como paramêtro para a página de login receber com o email já escrito no input

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,6 +1,8 @@
+import { registerRestaunt } from "@/api/register-restaurant";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
+import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 
 import { useForm } from "react-hook-form";
@@ -24,16 +26,25 @@ export function SignUp() {
     formState: { isSubmitting },
   } = useForm<SignUpForm>({});
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaunt,
+  });
+
   const navigate = useNavigate();
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await registerRestaurantFn({
+        email: data.email,
+        managerName: data.managerName,
+        phone: data.phone,
+        restaurantName: data.restaurantName,
+      });
 
       toast.success("Restaurante cadastrado com sucesso.", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       });
     } catch (error) {
