f334f0b7a07b84d0a8c0c9dac1dd97f2f02ccb4d
Ryan <ryangabriel_cap@hotmail.com>
2024-07-30 16:22:16 +0100

    feat: Adicionado informações do restaurante e do usuário buscando direto na nossa API, utilizando o useQuery para obter a função desejada, o estado de loading e a key para quando precisarmos usar essa função novamente em outros pedaços da nossa aplicação. Adicionado componente Skeleton enquanto isLoading não é cumprido.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,8 +8,23 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaunt } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaunt,
+    });
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -17,24 +32,37 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Ryan Martins</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            ryan@email.com
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
-        <DropdownMenuSeparator/>
+        <DropdownMenuSeparator />
         <DropdownMenuItem className="cursor-pointer">
-          <Building className="w-4 h-4 mr-2"/>
+          <Building className="mr-2 h-4 w-4" />
           <span>Perfil da loja</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400 cursor-pointer">
-          <LogOut className="w-4 h-4 mr-2"/>
+        <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
           <span>Sair</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
