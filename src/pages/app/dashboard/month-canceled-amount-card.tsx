fb2fa475907d31c70b82368117fee85d3dd844d4
Ryan <ryangabriel_cap@hotmail.com>
2024-08-19 22:07:24 +0100

    refactory: Alterando componente do Card de cancelamentos para os dados obtidos da API

--- src/pages/app/dashboard/month-canceled-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-amount-card.tsx
@@ -1,8 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
-import { useQuery } from "@tanstack/react-query";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export function MonthCanceledOrdersAmountCard() {
   const { data: monthCanceledOrdersAmount } = useQuery({
@@ -22,10 +22,10 @@ export function MonthCanceledOrdersAmountCard() {
         {monthCanceledOrdersAmount && (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {monthCanceledOrdersAmount.amount?.toLocaleString("pt-BR") || "N/A"}
+              {monthCanceledOrdersAmount.amount ? monthCanceledOrdersAmount.amount : "N/A"}
             </span>
             <p className="text-xs text-muted-foreground">
-              {monthCanceledOrdersAmount.diffFromLastMonth <= 0 ? (
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
                 <>
                   <span className="text-emerald-500 dark:text-emerald-400">
                     {monthCanceledOrdersAmount.diffFromLastMonth}%
@@ -42,7 +42,7 @@ export function MonthCanceledOrdersAmountCard() {
               )}
             </p>
           </>
-        ) }
+        )}
       </CardContent>
     </Card>
   );
