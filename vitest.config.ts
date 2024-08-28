c0477be9890ab1d309b0fb485fa4cf0d240ad85c
Ryan <ryangabriel_cap@hotmail.com>
2024-08-28 23:55:27 +0100

    fix: Corrigido bug de importação de "react" para todos os componentes, adicionando o plugins: [react()] ao vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest" />
+import { resolve } from "path";
 import { defineConfig } from "vite";
-
+import react from "@vitejs/plugin-react"
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     setupFiles: ["./test/setup.ts"],
-    environment: "happy-dom"
+    environment: "happy-dom",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
   },
 });
