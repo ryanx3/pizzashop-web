66e30f6b91465d18834c3ccefed919d4897fc07b
Ryan <ryangabriel_cap@hotmail.com>
2024-08-31 00:02:23 +0100

    feat: Alterando renderizaçao da main para depender do EnableMSW.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./app";
+import { EnableMsw } from "./api/mocks";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+EnableMsw().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
