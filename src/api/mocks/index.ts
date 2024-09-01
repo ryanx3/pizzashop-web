6159a2076871536bcb8423e08fbd711a88db737c
Ryan <ryangabriel_cap@hotmail.com>
2024-09-01 22:42:00 +0100

    feat: adicionar mock de autenticação ao setup do MSW

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,7 +1,8 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
+import { SignInMock } from "./sign-in-mock";
 
-export const worker = setupWorker();
+export const worker = setupWorker(SignInMock);
 
 export async function EnableMsw() {
   if (env.MODE !== "test") {
