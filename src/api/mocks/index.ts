11e9b18b6b44fac6e795f4126c4b5c1cbb50b148
Ryan <ryangabriel_cap@hotmail.com>
2024-09-02 23:59:59 +0100

    Adicionado funções aos testes

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,8 +2,13 @@ import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { SignInMock } from "./sign-in-mock";
 import { RegisterRestaurantMock } from "./register-restaurant-mock";
+import { GetDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 
-export const worker = setupWorker(SignInMock, RegisterRestaurantMock);
+export const worker = setupWorker(
+  SignInMock,
+  RegisterRestaurantMock,
+  GetDayOrdersAmountMock,
+);
 
 export async function EnableMsw() {
   if (env.MODE !== "test") {
