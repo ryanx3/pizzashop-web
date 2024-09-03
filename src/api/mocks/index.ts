cfa58537a8f41d89774e6252f82454592bf38cae
Ryan <ryangabriel_cap@hotmail.com>
2024-09-03 16:58:41 +0100

    feat: Adicionando mocks as solicitações HTTP do setupWorker do MSW

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,13 +1,23 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
-import { SignInMock } from "./sign-in-mock";
-import { RegisterRestaurantMock } from "./register-restaurant-mock";
-import { GetDayOrdersAmountMock } from "./get-day-orders-amount-mock";
+import { signInMock } from "./sign-in-mock";
+import { registerRestaurantMock } from "./register-restaurant-mock";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
+import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-mock";
+import { getMonthRevenueMock } from "./get-month-revenue-mock";
+import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
+import { getPopularProductsMock } from "./get-popular-products-mock";
 
 export const worker = setupWorker(
-  SignInMock,
-  RegisterRestaurantMock,
-  GetDayOrdersAmountMock,
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
 );
 
 export async function EnableMsw() {
