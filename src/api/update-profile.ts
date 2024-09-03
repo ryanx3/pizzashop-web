7f6cace23bcf7da952c9880027fdff35480e22a9
Ryan <ryangabriel_cap@hotmail.com>
2024-09-03 23:08:35 +0100

    feat: Criado teste para realizar uma atualização do perfil do restaurante na rota "/profile". refactor: Atualizando nome da função principal do updateProfile e exportando a tipagem.

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -1,13 +1,10 @@
 import { api } from "@/lib/axios";
 
-interface updateProfileResponse {
+export interface UpdateProfileResponse {
   name: string;
   description: string | null
 }
 
-export async function updateProfile({
-  name,
-  description,
-}: updateProfileResponse) {
+export async function updateProfile({ name, description }: UpdateProfileResponse) {
   await api.put("/profile", { name, description });
 }
