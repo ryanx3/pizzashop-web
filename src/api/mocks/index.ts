8770d9f00717c1bc522a54dd9dc2ac1c74da19b2
Ryan <ryangabriel_cap@hotmail.com>
2024-09-03 23:09:45 +0100

    feat: Adicionando funções de teste para execução com o setupWorker

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,6 +1,7 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { signInMock } from "./sign-in-mock";
+
 import { registerRestaurantMock } from "./register-restaurant-mock";
 import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
@@ -8,6 +9,9 @@ import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-mo
 import { getMonthRevenueMock } from "./get-month-revenue-mock";
 import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
 import { getPopularProductsMock } from "./get-popular-products-mock";
+import { getProfileMock } from "./get-profile-mock";
+import { updateProfileMock } from "./update-profile-mock";
+import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
 
 export const worker = setupWorker(
   signInMock,
@@ -18,6 +22,9 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getProfileMock,
+  updateProfileMock,
+  getManagedRestaurantMock,
 );
 
 export async function EnableMsw() {
